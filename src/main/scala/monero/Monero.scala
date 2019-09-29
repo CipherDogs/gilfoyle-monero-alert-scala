e9fd9b6d789f11697fae0328811fceec80cb770d
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-09-29 15:02:05 +0700

    Added GUI

--- src/main/scala/monero/Monero.scala
+++ src/main/scala/monero/Monero.scala
@@ -1,22 +1,18 @@
 package monero
 
+import java.awt.{Dimension, Image}
+import javax.imageio.ImageIO
+import javax.swing.{ImageIcon, JFrame, JLabel, WindowConstants}
 import org.json4s.JsonAST.{JString, JValue}
 import org.json4s.jackson.JsonMethods._
 import scalaj.http._
 
 object Monero {
   def main(args: Array[String]): Unit = {
-    val json = request()
 
-    val price = json \ "ticker" \ "price" match {
-      case JString(s) => s.toInt
-      case _ => 0
-    }
+    gui()
 
-    val change: Double = json \ "ticker" \ "change" match {
-      case JString(s) => s.toDouble
-      case _ => 0.0
-    }
+    val (price, change) = getData
 
     println(s"Price: $price")
     if (change < 0.0) {
@@ -27,5 +23,35 @@ object Monero {
   def request(): JValue = {
     val response: HttpResponse[String] = Http("https://api.cryptonator.com/api/ticker/xmr-usd").asString
     parse(response.body)
+  }
+
+  def getData: (Double, Double) = {
+    val json = request()
+
+    val price: Double = json \ "ticker" \ "price" match {
+      case JString(s) => s.toDouble
+      case _ => 0
+    }
+
+    val change: Double = json \ "ticker" \ "change" match {
+      case JString(s) => s.toDouble
+      case _ => 0.0
+    }
+
+    (price, change)
+  }
+
+  def gui(): Unit = {
+
+    val back = ImageIO.read(getClass.getResource("/back.png"))
+    val picLabel = new JLabel(new ImageIcon(back.getScaledInstance(500,500,Image.SCALE_FAST)))
+
+    val frame = new JFrame("Gilfoyle Monero Alert")
+    frame.getContentPane.add(picLabel)
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+    frame.setSize(new Dimension(500,500))
+    frame.setResizable(false)
+    frame.setLocationRelativeTo(null)
+    frame.setVisible(true)
   }
 }
