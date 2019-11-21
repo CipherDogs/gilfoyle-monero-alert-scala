f01ea3079f23f9d1c673643d6f1e5179d1422de2
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2019-11-21 21:10:42 +0700

    Code style

--- src/main/scala/monero/Main.scala
+++ src/main/scala/monero/Main.scala
@@ -27,26 +27,26 @@ class CheckPrice extends TimerTask {
 
 object Main extends App {
   val back: BufferedImage = ImageIO.read(getClass.getResource("/back.png"))
-  val picLabel = new JLabel(new ImageIcon(back.getScaledInstance(500,500,Image.SCALE_FAST)))
+  val picLabel            = new JLabel(new ImageIcon(back.getScaledInstance(500, 500, Image.SCALE_FAST)))
 
   val priceLabel = new JLabel("<html>Price<br>$</html>", SwingConstants.LEFT)
   priceLabel.setOpaque(true)
   priceLabel.setFont(priceLabel.getFont.deriveFont(24.0f))
   priceLabel.setBackground(Color.BLACK)
-  priceLabel.setForeground(new Color(227,29,26))
+  priceLabel.setForeground(new Color(227, 29, 26))
 
   val statLabel = new JLabel("<html>Height<br>0<br><br>Difficulty<br>0</html>", SwingConstants.LEFT)
   statLabel.setOpaque(true)
   statLabel.setFont(priceLabel.getFont.deriveFont(24.0f))
   statLabel.setBackground(Color.BLACK)
-  statLabel.setForeground(new Color(227,29,26))
+  statLabel.setForeground(new Color(227, 29, 26))
 
   val panel = new JPanel
   panel.setBackground(Color.BLACK)
   panel.setLayout(new BorderLayout)
   panel.add(priceLabel, BorderLayout.NORTH)
   panel.add(statLabel, BorderLayout.SOUTH)
-  panel.setPreferredSize(new Dimension(200,500))
+  panel.setPreferredSize(new Dimension(200, 500))
 
   val frame = new JFrame("Gilfoyle Monero Alert")
   frame.addWindowListener(new WindowAdapter {
@@ -58,7 +58,7 @@ object Main extends App {
   })
   frame.getContentPane.add(picLabel, BorderLayout.CENTER)
   frame.getContentPane.add(panel, BorderLayout.EAST)
-  frame.setSize(new Dimension(700,500))
+  frame.setSize(new Dimension(700, 500))
   frame.setResizable(false)
   frame.setLocationRelativeTo(null)
   frame.setVisible(true)
@@ -67,10 +67,10 @@ object Main extends App {
   timer.schedule(new CheckPrice, 0, 60000)
 
   def alertAudio(): Unit = {
-    val is = getClass.getResourceAsStream("/baaah.wav")
-    val buf = new BufferedInputStream(is)
+    val is    = getClass.getResourceAsStream("/baaah.wav")
+    val buf   = new BufferedInputStream(is)
     val audio = AudioSystem.getAudioInputStream(buf)
-    val clip = AudioSystem.getClip
+    val clip  = AudioSystem.getClip
     clip.open(audio)
     clip.start()
     Thread.sleep(2000)
--- src/main/scala/monero/Request.scala
+++ src/main/scala/monero/Request.scala
@@ -9,28 +9,28 @@ import scalaj.http.{Http, HttpResponse}
 object Request {
   def getPrice: Future[(Double, Double)] = Future {
     val response: HttpResponse[String] = Http("https://api.cryptonator.com/api/ticker/xmr-usd").asString
-    val json = parse(response.body)
+    val json                           = parse(response.body)
     val price: Double = json \ "ticker" \ "price" match {
       case JString(s) => s.toDouble
-      case _ => 0.0
+      case _          => 0.0
     }
     val change: Double = json \ "ticker" \ "change" match {
       case JString(s) => s.toDouble
-      case _ => 0.0
+      case _          => 0.0
     }
     (price, change)
   }
 
   def getStat: Future[(BigInt, BigInt)] = Future {
     val response: HttpResponse[String] = Http("https://moneroblocks.info/api/get_stats").asString
-    val json = parse(response.body)
+    val json                           = parse(response.body)
     val height: BigInt = json \ "height" match {
       case JInt(s) => s
-      case _ => 0
+      case _       => 0
     }
     val difficulty: BigInt = json \ "difficulty" match {
       case JInt(s) => s
-      case _ => 0
+      case _       => 0
     }
     (height, difficulty)
   }
